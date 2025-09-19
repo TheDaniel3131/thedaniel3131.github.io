f70be09d20a93c4bdca10dbbf66b1e9c10aad260
zephrous <danielpoh3131@gmail.com>
2025-09-19 23:33:20 +0800

    Re:Update About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,7 +33,7 @@ export default function About() {
 
   const stats = [
     {
-      label: "Working Years Experience",
+      label: "Years of Experience",
       value: "~1",
       icon: <Briefcase className="h-4 w-4 sm:h-5 sm:w-5" />,
     },
